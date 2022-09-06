5b1d3af66915cbeda54d2d8844e307d2b39f30e5
adachi <adachi@launchcraft.jp>
2022-09-06 19:50:21 +0900

    リファクタリング

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,27 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 use App\Http\Controllers\TodosController;
-// Route::get('/','TodosController@index');
-Route::get('/', [TodosController::class, 'index'])->name('Todos');
-Route::resource('todos', TodosController::class);
 
-Route::get('/test', function() {
-    return view('test');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::resource('todos', TodosController::class);
